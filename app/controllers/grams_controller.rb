afe1e6a0393b2fb185617a35dae7d72ed9fc90b9
Nick Parsley <nicholasparsley@gmail.com>
2019-12-05 01:33:12 +0000

    Adding a Gram Detail Page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,13 @@ class GramsController < ApplicationController
   def index
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
@@ -22,5 +29,4 @@ class GramsController < ApplicationController
   def gram_params
     params.require(:gram).permit(:message)
   end
-
 end
